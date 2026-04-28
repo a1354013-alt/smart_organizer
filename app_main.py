62bf250b46f6b0169c3d1645c96d2f6c0a72a5d9
YangWen Luo <a1354013@gm.ncue.edu.tw>
2026-04-28 10:41:58 +0800

    fix: remove BOM from source files

--- app_main.py
+++ app_main.py
@@ -1,4 +1,4 @@
-﻿import streamlit as st
+import streamlit as st
 import os
 import logging
 from pathlib import Path
--- create_release_zip.ps1
+++ create_release_zip.ps1
@@ -1,4 +1,4 @@
-﻿param(
+param(
     [string]$OutputDir = "release",
     [string]$ZipName = ""
 )
